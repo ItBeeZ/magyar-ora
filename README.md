b8f72af34a5898b5b2b5ea05031ba220a6622781
ItsBeeZ <probaemail2023@gmail.com>
2023-03-23 13:37:56 +0100

    A sima alpha kész, viszont változtatások érintik őt a jövőben

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # magyar-ora
 Magyar órai felelésekre való név generátor, két fájl tartalmaz az egyik szint tisztai név generátor a másik pedig verssel együtti név generátor
+
+*VÁLTOZTATÁSOK A JÖVŐBEN*
+    1.  Megkérdezi hány hiányzó van és a neveket is 
+        majd a bekért neveket automatikusan kiveszi a listából
+        így azok nem kerülnek kisorsolásra
+
+    2. Az első következő és utolsó felelőt 
+       kiírja -- Kész!
+
+    3. Valami kisebb 'díszitést'
+       a printekre
--- gen1.py
+++ gen1.py
@@ -1,1 +1,22 @@
-lista = []
+import random
+from colorama import Fore, Style
+
+lista = ['Kőszegi Bence', 'Czene Bálint', 'Kurtucz Krisztián']
+
+
+gen = random.choice(lista)
+
+while len(lista) > 0:
+    beker = input('A név sorsoláshoz kérem nyomjon egy Enter-t\t')
+    if len(lista) == 1:
+        print(f'A UTOLSÓ FELELŐ {lista[0]} LESZ\n')
+        print(f"{Fore.GREEN}Ez a szöveg zöld színnel jelenik meg!{Style.RESET_ALL}")
+        break
+    elif gen in lista:
+        print(f'A KÖVETKEZŐ FELELŐ {gen} LESZ\n')
+        lista.remove(gen)
+        gen = random.choice(lista)
+    if not gen in lista:
+        gen = random.choice(lista)
+        
+        
