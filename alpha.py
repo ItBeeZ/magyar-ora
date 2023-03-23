9926fbecd54d233330e219c5feac6e10ab1c9db4
ItsBeeZ <probaemail2023@gmail.com>
2023-03-23 18:27:07 +0100

    Színek kivéve --> Uppercase név kiirás

--- alpha.py
+++ alpha.py
@@ -3,24 +3,25 @@ from colorama import Fore, Style
 
 lista = ['Szalay Balázs', 'Czene Bálint', 'Hrabovszki Bálint', 'Erdélyi Barnabás', 'Kőszegi Bence', 'Lajos Boton', 'Kovács Dávid', 'Eszenyi Panna', 'Zátonyi Gábor', 'Gál Gréta', 'Szijjárto Gyula', 'Halász Sándor', 'Dudaszeg Krisztián', 'Kurtucz Krisztián', 'Nagy László Krisztián', 'Gaál Levente', 'Varga Levente', 'Madarász Máté', 'Zsuzsa Mihály', 'Miklos Dávid', 'Molnár Norman', 'Szendy Norbert', 'Beke Richárd', 'Szegedi Richárd', 'Rostás Szilárd', 'Tiba Bálint', 'Vicze Sándor', 'Balassa Zalán', 'Futaki Zoltán', 'Kiss Zoltán', 'Somogyi Zsombor']
 
+upper = [i.upper() for i in lista]
 
-gen = random.choice(lista)
+gen = random.choice(upper)
 
-while len(lista) > 0:
-    #print(len(lista))
+while len(upper) > 0:
+    #print(len(upper))
     beker = input('A név sorsoláshoz kérem nyomjon egy Enter-t\t')
-    if len(lista) == 1:
-        print(f'Az utolsó felelő \033[95m{lista[0]}\033[0m lesz\n')
+    if len(upper) == 1:
+        print(f'Az utolsó felelő {upper[0]} lesz\n')
         break
-    elif gen in lista:
-        if len(lista) == 31:
-            print(f'Az első felelő \033[95m{gen}\033[0m lesz\n')
-            lista.remove(gen)
-        elif len(lista) < 31:
-            print(f'Az következő felelő \033[95m{gen}\033[0m lesz\n')
-            lista.remove(gen)
-        gen = random.choice(lista)
-    if not gen in lista:
-        gen = random.choice(lista)
+    elif gen in upper:
+        if len(upper) == 31:
+            print(f'Az első felelő {gen} lesz\n')
+            upper.remove(gen)
+        elif len(upper) < 31:
+            print(f'Az következő felelő {gen} lesz\n')
+            upper.remove(gen)
+        gen = random.choice(upper)
+    if not gen in upper:
+        gen = random.choice(upper)
         
 
