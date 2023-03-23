b323b775f799835d76fe47d77cbc4fad033f8fc5
ItsBeeZ <probaemail2023@gmail.com>
2023-03-23 14:12:45 +0100

    A lista kibővült az osztály neveivel

--- gen1.py
+++ gen1.py
@@ -1,7 +1,7 @@
 import random
 from colorama import Fore, Style
 
-lista = ['Kőszegi Bence', 'Czene Bálint', 'Kurtucz Krisztián']
+lista = ['Szalay Balázs', 'Czene Bálint', 'Hrabovszki Bálint', 'Erdélyi Barnabás', 'Kőszegi Bence', 'Lajos Boton', 'Kovács Dávid', 'Eszenyi Panna', 'Zátonyi Gábor', 'Gál Gréta', 'Szijjárto Gyula', 'Halász Sándor', 'Dudaszeg Krisztián', 'Kurtucz Krisztián', 'Nagy László Krisztián', 'Gaál Levente', 'Varga Levente', 'Madarász Máté', 'Zsuzsa Mihály', 'Miklos Dávid', 'Molnár Norman', 'Szendy Norbert', 'Beke Richárd', 'Szegedi Richárd', 'Rostás Szilárd', 'Tiba Bálint', 'Vicze Sándor', 'Balassa Zalán', 'Futaki Zoltán', 'Kiss Zoltán', 'Somogyi Zsombor']
 
 
 gen = random.choice(lista)
@@ -13,11 +13,11 @@ while len(lista) > 0:
         print(f'Az utolsó felelő \033[95m{lista[0]}\033[0m lesz\n')
         break
     elif gen in lista:
-        if len(lista) == 3:
-            print(f'Az első \033[95m{gen}\033[0m lesz\n')
+        if len(lista) == 31:
+            print(f'Az első felelő \033[95m{gen}\033[0m lesz\n')
             lista.remove(gen)
-        elif len(lista) < 3:
-            print(f'Az következő \033[95m{gen}\033[0m lesz\n')
+        elif len(lista) < 31:
+            print(f'Az következő felelő \033[95m{gen}\033[0m lesz\n')
             lista.remove(gen)
         gen = random.choice(lista)
     if not gen in lista:
