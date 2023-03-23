d72ece5f61aaa1df0a0b11146038ddfb71e55ca1
ItsBeeZ <probaemail2023@gmail.com>
2023-03-23 13:53:31 +0100

    A felsorolt változtatások megtörténtek

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 Magyar órai felelésekre való név generátor, két fájl tartalmaz az egyik szint tisztai név generátor a másik pedig verssel együtti név generátor
 
 *VÁLTOZTATÁSOK A JÖVŐBEN*
+    
+    
     1.  Megkérdezi hány hiányzó van és a neveket is 
         majd a bekért neveket automatikusan kiveszi a listából
         így azok nem kerülnek kisorsolásra
--- gen1.py
+++ gen1.py
@@ -7,16 +7,20 @@ lista = ['Kőszegi Bence', 'Czene Bálint', 'Kurtucz Krisztián']
 gen = random.choice(lista)
 
 while len(lista) > 0:
+    #print(len(lista))
     beker = input('A név sorsoláshoz kérem nyomjon egy Enter-t\t')
     if len(lista) == 1:
-        print(f'A UTOLSÓ FELELŐ {lista[0]} LESZ\n')
-        print(f"{Fore.GREEN}Ez a szöveg zöld színnel jelenik meg!{Style.RESET_ALL}")
+        print(f'Az utolsó felelő \033[95m{lista[0]}\033[0m lesz\n')
         break
     elif gen in lista:
-        print(f'A KÖVETKEZŐ FELELŐ {gen} LESZ\n')
-        lista.remove(gen)
+        if len(lista) == 3:
+            print(f'Az első \033[95m{gen}\033[0m lesz\n')
+            lista.remove(gen)
+        elif len(lista) < 3:
+            print(f'Az következő \033[95m{gen}\033[0m lesz\n')
+            lista.remove(gen)
         gen = random.choice(lista)
     if not gen in lista:
         gen = random.choice(lista)
         
-        
+
